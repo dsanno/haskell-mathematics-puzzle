c817b8a155eb1278a0c06f6df5c99c1a04b19d08
Daiki.Sanno <daiki.sanno@gmail.com>
2018-02-06 00:33:37 +0900

    CutStick.solve may fail, so returns Maybe.

--- src/04-cut-stick/CutStick.hs
+++ src/04-cut-stick/CutStick.hs
@@ -8,12 +8,12 @@ import qualified Data.List as List
 cut :: Int -> Int -> Int
 cut n i = min n (2 ^ i)
 
-estimate :: Int -> Int -> Int
+estimate :: Int -> Int -> Maybe Int
 estimate len n =
   let xs = scanl (\s i -> s + cut n i) 0 [1..]
-  in maybe (-1) (+ 1) $ List.findIndex (>= len - 1) xs
+  in (+ 1) <$> List.findIndex (>= len - 1) xs
 
-solve :: [Int]
+solve :: [Maybe Int]
 solve = [ estimate 20 3
         , estimate 100 5
         ]
