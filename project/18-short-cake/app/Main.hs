48c84235989cf11510c265b33a35f7af8aa6e6c2
Daiki.Sanno <daiki.sanno@gmail.com>
2018-02-16 01:01:34 +0900

    Fix problem 18.

--- project/18-short-cake/app/Main.hs
+++ project/18-short-cake/app/Main.hs
@@ -1,25 +1,28 @@
 module Main where
 
-import qualified Data.List  as List
 import qualified Data.Maybe as Maybe
 import qualified Data.Set   as Set
-
-import qualified Lib
-
 
 isSquare :: Int -> Bool
 isSquare n = (round . sqrt . fromIntegral) n ^ 2 == n
 
 findSquareNeighbors' :: Set.Set Int -> Int -> Int -> Maybe [Int]
-findSquareNeighbors' ns firstNum lastNum =
-  let ms = filter (isSquare . (+ lastNum)) $ Set.toList ns
-      ys = Maybe.mapMaybe (\m -> findSquareNeighbors' (Set.delete m ns) firstNum m) ms
-  in case ys of
-    []    -> Nothing
-    (x:_) -> Just x
+findSquareNeighbors' ns firstNum lastNum
+  | Set.null ns && isSquare (firstNum + lastNum) = Just [lastNum]
+  | otherwise =
+    let ms = filter (isSquare . (+ lastNum)) $ Set.toList ns
+        ys = Maybe.mapMaybe (\m -> findSquareNeighbors' (Set.delete m ns) firstNum m) ms
+    in case ys of
+      []    -> Nothing
+      (x:_) -> Just $ lastNum:x
 
 findSquareNeighbors :: Int -> Maybe [Int]
-findSquareNeighbors n = findSquareNeighbors' (Set.fromList [2..n]) 1 1
+findSquareNeighbors n =
+  let ns = Set.fromList [2..n]
+  in findSquareNeighbors' ns 1 1
 
 main :: IO ()
-main = print . head $ Maybe.mapMaybe findSquareNeighbors [2..]
+main = do
+  let xs = head $ Maybe.mapMaybe findSquareNeighbors [2..]
+  print xs
+  print $ length xs
