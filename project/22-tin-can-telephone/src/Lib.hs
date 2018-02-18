5379dee554eb4fa78b970285c74e760bd3d2559f
Daiki.Sanno <daiki.sanno@gmail.com>
2018-02-19 00:02:54 +0900

    Use simpler algorithm.

--- project/22-tin-can-telephone/src/Lib.hs
+++ project/22-tin-can-telephone/src/Lib.hs
@@ -8,15 +8,12 @@ import qualified Data.Map.Lazy as Map
 
 countPatterns' :: Map.Map Int Int -> Int -> Int
 countPatterns' ps n =
-  let x = ps ! (n - 2) * 2
-      y = sum $ map (\m -> ps ! m * ps ! (n - m - 2)) [2, 4..(n - 4)]
-  in x + y
+  sum $ map (\m -> ps ! m * ps ! (n - m - 2)) [0, 2..(n - 2)]
 
 countPatterns :: Int -> Int
 countPatterns 0 = 0
-countPatterns 2 = 1
 countPatterns n
   | odd n = 0
   | otherwise =
-      let ps = foldl (\qs m -> Map.insert m (countPatterns' qs m) qs) (Map.singleton 2 1) [4, 6..n]
+      let ps = foldl (\qs m -> Map.insert m (countPatterns' qs m) qs) (Map.singleton 0 1) [2, 4..n]
       in ps ! n
