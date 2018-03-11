6dae471237cbccceb2ac3a822d28b0c75de30822
Daiki.Sanno <daiki.sanno@gmail.com>
2018-03-11 13:26:07 +0900

    Use cache.

--- project/31-shortest-path/src/Lib.hs
+++ project/31-shortest-path/src/Lib.hs
@@ -1,45 +1,41 @@
 module Lib
-    ( allPaths
-    , countUnoverwrappedPaths
+    ( countUnoverwrappedPaths
     ) where
 
-type Point = (Int, Int)
-type Segment = (Point, Point)
-type Path = [Segment]
 
-allPaths' :: Point -> Point -> [Path]
-allPaths' from@(fx, fy) to@(tx, ty)
-  | fx > tx || fy > ty = []
-  | from == to = [[]]
-  | fx == tx = downPaths
-  | fy == ty = rightPaths
-  | otherwise = rightPaths ++ downPaths
-  where
-    right = (fx + 1, fy)
-    down = (fx, fy + 1)
-    rightPaths = (:) <$> [(from, right)] <*> allPaths' right to
-    downPaths = (:) <$> [(from, down)] <*> allPaths' down to
+import           Control.Monad       (mapM)
+import           Control.Monad.State
+import qualified Data.Map.Lazy       as Map
 
-allPaths :: Int -> Int -> [Path]
-allPaths w h = allPaths' (0, 0) (w, h)
+type Cache = Map.Map (Int, Int, Int) Int
 
-overwrap :: Path -> Path -> Bool
-overwrap xs ys = any (uncurry (==)) $ zip xs ys
+withCache :: (Int, Int, Int) -> State Cache Int -> State Cache Int
+withCache key f = do
+  cache <- get
+  case Map.lookup key cache of
+    Just m -> return m
+    _      -> do
+                m <- f
+                cache' <- get
+                put $ Map.insert key m cache'
+                return m
 
-countPaths :: Int -> Int -> Int -> Int
-countPaths w 1 0 = 2
-countPaths w 1 returnY = 1
+countPaths :: Int -> Int -> Int -> State Cache Int
+countPaths w 1 0 = return 2
+countPaths w 1 returnY = return 1
 countPaths 1 h returnY
-  | h == returnY = returnY
-  | otherwise = returnY + 2
-countPaths w h 0 = rightPathNum + downPathNum
-  where
-    rightPathNum = sum $ map (countPaths (w - 1) h) [1..h]
-    downPathNum = sum $ map (countPaths (h - 1) w) [1..w]
-countPaths w h returnY = rightPathNum + downPathNum
-  where
-    rightPathNum = sum $ map (countPaths (w - 1) h) [returnY..h]
-    downPathNum = countPaths w (h - 1) (returnY - 1)
+  | h == returnY = return returnY
+  | otherwise = return $ returnY + 2
+countPaths w h returnY = withCache (w, h, returnY) $
+  case returnY of
+    0 -> do
+      rightNums <- mapM (countPaths (w - 1) h) [1..h]
+      downNums <- mapM (countPaths (h - 1) w) [1..w]
+      return $ sum rightNums + sum downNums
+    _ -> do
+      rightNums <- mapM (countPaths (w - 1) h) [returnY..h]
+      downNum <- countPaths w (h - 1) (returnY - 1)
+      return $ sum rightNums + downNum
 
 countUnoverwrappedPaths :: Int -> Int -> Int
-countUnoverwrappedPaths w h = countPaths w h 0
+countUnoverwrappedPaths w h = evalState (countPaths w h 0) Map.empty
