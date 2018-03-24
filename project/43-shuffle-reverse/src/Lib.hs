3cb3101f7f46c1def2592089b46aa21c33e51320
Daiki.Sanno <daiki.sanno@gmail.com>
2018-03-24 16:20:58 +0900

    Use bidirectional search.

--- project/43-shuffle-reverse/src/Lib.hs
+++ project/43-shuffle-reverse/src/Lib.hs
@@ -17,41 +17,28 @@ import qualified Data.Set            as Set
 -- 4
 
 minShuffleCount :: Int -> Int
-minShuffleCount n = evalState (minShuffleCountWithCache size offsets target [[1..n]]) Set.empty
+minShuffleCount n = evalState (minShuffleCountWithCache size offsets targets [[1..n]]) (Set.empty, Set.empty)
   where
     size = div n 2
     offsets = [1..(n - size)]
-    target = reverse [1..n]
+    targets = [reverse [1..n]]
 
-minShuffleCountWithCache :: (Ord a) => Int -> [Int] -> [a] -> [[a]] -> State (Set [a]) Int
-minShuffleCountWithCache size offsets target xs =
-  if target `elem` ys
-  then return 1
-  else do
-    cache <- get
-    let c = Set.fromList ys
-        nexts = Set.toList $ Set.difference c cache
-    put $ Set.union c cache
-    count <- minShuffleCountWithCache size offsets target nexts
-    return $ count + 1
+minShuffleCountWithCache :: (Ord a) => Int -> [Int] -> [[a]] -> [[a]] -> State (Set [a], Set [a]) Int
+minShuffleCountWithCache size offsets targets xs
+  | any (`Set.member` s1) targets = return 1
+  | any (`Set.member` s2) s1 = return 2
+  | otherwise = do
+      (c1, c2) <- get
+      let n1 = Set.difference s1 c1
+      let n2 = Set.difference s2 c2
+      put (Set.union c1 n1, Set.union c2 n2)
+      count <- minShuffleCountWithCache size offsets (Set.toList n2) (Set.toList n1)
+      return $ count + 2
   where
-    ys = shuffle size <$> offsets <*> xs
-
-
--- | find minimum Shuffle Count
---
--- >>> minShuffleCount' 2
--- Just 1
--- >>> minShuffleCount' 4
--- Just 4
-
-minShuffleCount' :: Int -> Maybe Int
-minShuffleCount' n = List.findIndex (target `elem`) steps
-  where
-    size = div n 2
-    target = reverse [1..n]
-    step lists = shuffle size <$> [1..(n - size)] <*> lists
-    steps = iterate step [[1..n]]
+    ys1 = shuffle size <$> offsets <*> xs
+    ys2 = reverseShuffle size <$> offsets <*> targets
+    s1 = Set.fromList ys1
+    s2 = Set.fromList ys2
 
 -- | shuffle list
 --
@@ -65,3 +52,16 @@ shuffle size offset xs = b ++ a ++ c
   where
     (a, rest) = List.splitAt offset xs
     (b, c) = List.splitAt size rest
+
+-- | reverse shuffle list
+--
+-- >>> reverseShuffle 1 0 [1, 2]
+-- [1,2]
+-- >>> reverseShuffle 2 1 [1..4]
+-- [3,1,2,4]
+
+reverseShuffle :: Int -> Int -> [a] -> [a]
+reverseShuffle size offset xs = b ++ a ++ c
+  where
+    (a, rest) = List.splitAt size xs
+    (b, c) = List.splitAt offset rest
